86fd73a6d5434cba9c7923b35eab77e8197905fd
albertoornelasdev <albertoornelasdev@gmail.com>
2021-08-10 22:22:17 -0700

    UsersList component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import './style.css';
-import useUsers from './hooks/useUsers';
+import UsersList from './components/UsersList';
 
 const App = () => {
-  const users = useUsers();
-
-  console.log(users);
-
   return (
     <div>
       Hello
-      {users.map(item => (
-        <div>
-          <img src={item.picture.thumbnail} />
-          <h1>
-            Name: {item.name.first} {item.name.last}
-          </h1>
-          <h2>Email: {item.email}</h2>
-          <h2>Age: {item.dob.age}</h2>
-          <h2>Phone: {item.phone}</h2>
-          <h2>Country: {item.location.country}</h2>
-          <h2>Gender: {item.gender}</h2>
-        </div>
-      ))}
+      <UsersList />
     </div>
   );
 };
---
+++ src/components/UsersList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import useUsers from '../hooks/useUsers';
+
+const UsersList = () => {
+  const users = useUsers();
+
+
+  return (
+    <div>
+      {users.map(item => (
+        <div>
+          <img src={item.picture.thumbnail} />
+          <h1>
+            Name: {item.name.first} {item.name.last}
+          </h1>
+          <h2>Email: {item.email}</h2>
+          <h2>Age: {item.dob.age}</h2>
+          <h2>Phone: {item.phone}</h2>
+          <h2>Country: {item.location.country}</h2>
+          <h2>Gender: {item.gender}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UsersList;
